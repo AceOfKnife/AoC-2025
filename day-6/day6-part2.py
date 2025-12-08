cf582ae2a8ff0175015cea3e27c30edd88ade4f0
Vincent Nguyen <vincent.nguyen@hotmail.co.uk>
2025-12-08 11:20:29 +0000

    Day 6

--- day-6/day6-part2.py
+++ day-6/day6-part2.py
@@ -1,11 +1,38 @@
+def calculateColumn(mathHomework, end):
+    start = end - 1
+    nums = []
+    while start >= 0:
+        num = 0
+        for i in range(len(mathHomework) - 1):
+            if mathHomework[i][start] != ' ':
+                num *= 10
+                num += int(mathHomework[i][start])
+        nums.append(num)
+        if mathHomework[-1][start] == '*':
+            res = 1
+            for num in nums:
+                res *= num
+            return res, start - 1
+        if mathHomework[-1][start] == '+':
+            return sum(nums), start - 1
+        start -= 1
+    return None
+
 def main():
     
-    numbers = []
+    mathHomework = []
 
-    with open("day6-test.txt") as mathHomework:
-        print(len(mathHomework))
-        for line in mathHomework:
-            print(line.split(" "))
+    with open("day6.txt") as mathInput:
+        for line in mathInput:
+            mathHomework.append(line.replace("\n", ""))
+    
+    end = len(mathHomework[0])
+    total = []
+    while end > 0:
+        res, end = calculateColumn(mathHomework, end)
+        total.append(res)
+    print("Part 2:", sum(total))
+
         
 if __name__=="__main__":
     main()
